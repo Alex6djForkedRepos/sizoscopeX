318e483ce3e00f8cca770733bb1e45bbd32665b2
Steven He <hez2010@outlook.com>
2024-11-13 22:24:33 +0900

    Adding support for NativeAOT-LLVM

--- sizoscopeX.Browser/wwwroot/main.js
+++ sizoscopeX.Browser/wwwroot/main.js
@@ -1,4 +1,4 @@
-﻿import { dotnet } from './dotnet.js'
+import { dotnet } from './dotnet.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
@@ -6,8 +6,9 @@ if (!is_browser) throw new Error(`Expected to be running in a browser`);
 const dotnetRuntime = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
+    .withMainAssembly("sizoscopeX.Browser")
     .create();
 
 const config = dotnetRuntime.getConfig();
 
-await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [window.location.search]);
+await dotnetRuntime.runMain(config.mainAssemblyName, [globalThis.location.href]);
